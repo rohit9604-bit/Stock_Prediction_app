8eb1352c821c4a7f2846369623234e292d68d483
rohit9604-bit <rohitmandan07@gmail.com>
2025-09-26 18:14:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './sources/routes/authRoutes.js'; 
+import authRoutes from './authRoutes.js'; 
 
 dotenv.config({ path: './config.env' });
 
@@ -16,4 +16,5 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Auth API running on http://localhost:${port}`);
 });
+
 
